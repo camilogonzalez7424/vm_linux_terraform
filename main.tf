9d8a09af4d45caa91d77ba01492beca6d7572e60
Camilo González V. <gonzalezcamilo508@gmail.com>
2024-03-13 02:34:16 +0000

    add main.tf

---
+++ main.tf
@@ -0,0 +1,68 @@
+provider "azurerm" {
+  features {}
+}
+
+# Se crea el grupo de recursos, al cual se asociarán los demás recursos
+resource "azurerm_resource_group" "rgC" {
+  name     = var.name_function
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rgC.location
+  resource_group_name = azurerm_resource_group.rgC.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rgC.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.rgC.location
+  resource_group_name = azurerm_resource_group.rgC.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.rgC.location
+  resource_group_name   = azurerm_resource_group.rgC.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_D2_v3"  # Cambiar el tamaño de la máquina virtual
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
