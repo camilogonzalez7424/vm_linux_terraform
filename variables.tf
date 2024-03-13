ab40b895b28335ba952889bb600e0ec58e11e650
Camilo González V. <gonzalezcamilo508@gmail.com>
2024-03-13 02:34:32 +0000

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  type        = string
+  description = "Prefix for resource names"
+}
+
+variable "name_function" {
+  type        = string
+  description = "Name of the function"
+}
+
+variable "location" {
+  type        = string
+  description = "Location for the Azure resources"
+}
